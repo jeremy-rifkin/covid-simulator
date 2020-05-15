3142b9b4e163463c639e02407405735ded7c2777
Jeremy Rifkin <rifkin.jer@gmail.com>
2020-05-15 12:50:30 -0600

    basic simulation

--- index.js
+++ index.js
@@ -17,16 +17,31 @@ function screentopx(x, y) {
 		pxh * (1 - (y + (h / 2)) / h)
 	];
 }
-		
+
+let red = "#e56a59",
+	grey = "#c6c6c6",
+	yellow = "#e8e388",
+	blue = "#7dcef1";
+
+let states = {
+	vulnerable: 0,
+	infected: 1,
+	recovered: 2
+};
+
+let recovery_time = 1000 * 10;
 
 class Ball {
 	constructor() {
-		this.x = 0;
-		this.y = 0;
-		this.vx = 20 + Math.random() * 10 - 5;
-		this.vy = 5 + Math.random() * 10 - 5;
-		this.m = 10 + Math.random() * 20;
-		this.r = 2 + Math.random() * 5;
+		this.r = 2;
+		this.m = 10;
+		this.x = -w/2 + this.r + Math.random() * (w - 2 * this.r);
+		this.y = -h/2 + this.r + Math.random() * (h - 2 * this.r);
+		let theta = Math.random() * 2 * Math.PI;
+		this.vx = 5 * Math.cos(theta);
+		this.vy = 5 * Math.sin(theta);
+		this.state = Math.random() < 0.05 ? states.infected : states.vulnerable;
+		this.infected_time = this.state == states.infected ? Date.now() : null;
 	}
 	update() {
 		this.x += this.vx * dt;
@@ -45,6 +60,11 @@ class Ball {
 			this.y = h / 2 - this.r;
 			this.vy *= -1;
 		}
+		// check our infection time
+		if(this.state == states.infected && Date.now() - this.infected_time >= recovery_time) {
+			this.state = states.recovered;
+			this.infected_time = null;
+		}
 	}
 	rotate(v, theta) {
 		return [
@@ -55,8 +75,9 @@ class Ball {
 	updateAgainst(obj) {
 		let dx = obj.x - this.x,
 			dy = obj.y - this.y;
-		
+		// check collision
 		if(dx*dx + dy*dy <= (obj.r + this.r) * (obj.r + this.r)) {
+			// do elastic
 			let dvx = this.vx - obj.vx,
 				dvy = this.vy - obj.vy;
 			// if dot product of the velocity vector and vector between balls is negative, they're
@@ -81,10 +102,33 @@ class Ball {
 				obj.vx = v2[0];
 				obj.vy = v2[1];
 			}
+
+			// do infection
+			if(this.state == states.vulnerable && obj.state == states.infected) {
+				this.state = states.infected;
+				this.infected_time = Date.now();
+			}
+			// both ways
+			if(obj.state == states.vulnerable && this.state == states.infected) {
+				obj.state = states.infected;
+				obj.infected_time = Date.now();
+			}
 		}
 	}
 	draw() {
-		ctx.fillStyle = "blue";
+		switch(this.state) {
+			case states.vulnerable:
+				ctx.fillStyle = grey;
+				break;
+			case states.infected:
+				ctx.fillStyle = red;
+				break;
+			case states.recovered:
+				ctx.fillStyle = blue;
+				break;
+			default:
+				throw "oops";
+		}
 		ctx.beginPath();
 		ctx.arc(...screentopx(this.x, this.y), this.r * pxperunit, 0, 2 * Math.PI);
 		//console.log(...screentopx(this.x, this.y), this.r * pxperunit, 0, 2 * Math.PI);
@@ -93,10 +137,6 @@ class Ball {
 }
 
 let scene = [];
-
-for(let i = 0; i < 40; i++) {
-	scene.push(new Ball);
-}
 
 function update() {
 	for(let i = 0; i < scene.length; i++) {
@@ -134,4 +174,11 @@ function resize() {
 }
 resize();
 
-window.requestAnimationFrame(loop);
+function init() {
+	for(let i = 0; i < 40; i++) {
+		scene.push(new Ball);
+	}
+	window.requestAnimationFrame(loop);
+}
+
+init();
